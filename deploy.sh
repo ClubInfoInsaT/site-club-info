a917c073ff4dcdb3c8cfb7eba3b8c3a342627c03
Manah7 <manah@gmx.com>
2022-01-19 11:06:43 +0100

    [rebase] merging old changes, rebased b/c merge hell Co-authored-by: Shinyhero36 <Shinyhero36@users.noreply.github.com>

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ "$1" ]
 then
-  scp location.png logo.png styles.css favicon.ico index.html "$1"@etud.insa-toulouse.fr:public_html/club-info/
+  scp src/* "$1"@etud.insa-toulouse.fr:public_html/club-info/
 else
   echo "Command: 'deploy.sh username' "
 fi
