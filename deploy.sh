52067cf482567b216ae72a8d1122eb0c32e2fd49
Gérald LEBAN <gerald.leban0@gmail.com>
2022-02-07 14:51:53 +0100

    Recursive copy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ "$1" ]
 then
-  scp src/* "$1"@etud.insa-toulouse.fr:public_html/club-info/
+  scp -r src/* "$1"@etud.insa-toulouse.fr:public_html/club-info/
 else
   echo "Command: 'deploy.sh username' "
 fi
